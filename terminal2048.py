6b5433e113591fefcbc14c5a5bff999477eaad4a
Alex Gerdom <alexgerdom@gmail.com>
2016-08-18 18:29:45 -0400

    initial commit

--- terminal2048.py
+++ terminal2048.py
@@ -1,20 +1,14 @@
 from msvcrt import getch
 from time import sleep
-from config import KEYMAP,WIN,LOSE,DEFAULT_BOARD
+from config import KEYMAP, WIN, LOSE, DEFAULT_BOARD
 import gamelogic
+from displaylogic import print_board
 
-def print_board(board):
-    btwn = "+-----"*4 + "+"
-    line = ("|{:^5}"*4) + "|"
-    for row in board:
-        print(btwn)
-        print(line.format(*row))
-    print(btwn)
 
 def play(state=None, delay=.25):
     if state is None:
         state = gamelogic.add_next_piece(DEFAULT_BOARD)
-    while state not in (WIN,LOSE):
+    while state not in (WIN, LOSE):
         print_board(state)
         move = None
         while not move:
@@ -22,8 +16,9 @@ def play(state=None, delay=.25):
             if m_ in KEYMAP:
                 move = KEYMAP[m_]
             sleep(delay)
-        state = gamelogic.move(state,move)
+        state = gamelogic.move(state, move)
     print_board(state)
+
 
 if __name__ == "__main__":
     play()
